8f96973e8dbe1453ae9eacfc58cf47d00dee648c
Wizzel1 <r_christian@gmx.de>
2023-09-23 15:03:02 +0200

    Fix server env variable load

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,22 +1,22 @@
-import { ENV } from '$lib/server/env';
-import type { Database } from '$lib/supabase-types';
-import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
-import type { LayoutLoad } from './$types';
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public"
+import type { Database } from "$lib/supabase-types"
+import { createSupabaseLoadClient } from "@supabase/auth-helpers-sveltekit"
 
-export const load: LayoutLoad = (async ({ fetch, data, depends }) => {
-    depends('supabase:auth')
+export const load = async ({ fetch, data, depends }) => {
+    depends("supabase:auth")
 
     const supabase = createSupabaseLoadClient<Database>({
-        supabaseUrl: ENV.PUBLIC_SUPABASE_URL,
-        supabaseKey: ENV.PUBLIC_SUPABASE_ANON_KEY,
+        supabaseUrl: PUBLIC_SUPABASE_URL,
+        supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
         event: { fetch },
         serverSession: data.session,
-    });
-
-    const { data: { session } } = await supabase.auth.getSession();
+    })
+    const {
+        data: { session },
+    } = await supabase.auth.getSession()
 
     return {
         supabase,
         session,
-    };
-});
+    }
+}
