30a3f3c41ff344f08432010f816ee685d3b92ec0
Wizzel1 <r_christian@gmx.de>
2023-09-27 12:45:56 +0200

    Format

--- src/lib/server/stripe.ts
+++ src/lib/server/stripe.ts
@@ -1,6 +1,6 @@
-import Stripe from "stripe";
-import { ENV } from "./env";
+import Stripe from "stripe"
+import { ENV } from "./env"
 
 export const stripe = new Stripe(ENV.STRIPE_SECRET_KEY, {
-    apiVersion: "2023-08-16",
+	apiVersion: "2023-08-16"
 })
