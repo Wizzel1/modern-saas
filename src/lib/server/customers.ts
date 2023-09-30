1127947afc22b7a32cb52dd11bbb4fb1ce4a656d
Wizzel1 <r_christian@gmx.de>
2023-09-30 13:51:49 +0200

    Remove testClock

--- src/lib/server/customers.ts
+++ src/lib/server/customers.ts
@@ -37,14 +37,9 @@ export async function getCustomerBillingRecord(userId: string) {
 		throw userError || new Error("User not found")
 	}
 
-	const testClock = await stripe.testHelpers.testClocks.create({
-		frozen_time: Math.floor(Date.now() / 1000)
-	})
-
 	const stripeCustomer = await stripe.customers.create({
 		email: userData.user.email,
-		metadata: { userId: userId },
-		test_clock: testClock.id
+		metadata: { userId: userId }
 	})
 	if (!stripeCustomer) throw new Error("Failed to create Stripe customer")
 
